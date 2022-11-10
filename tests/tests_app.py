9d8b2fdc291ca1dcdaab9c07b4611c38da700d65
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-11-10 15:39:05 +0700

    Update tests_app.py

--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,17 +1,17 @@
 import pytest
 from mymodule.funcs import multiply, divide
 
-def dikalikan_dua():
-    return multiply()
-
-def dibagi_dua(x):
-    return divide()
-
+@pytest.fixture
 def numbers():
     a = 10
     b = 20
     return [a,b]
 
+def dikalikan_dua():
+    return multiply()
+
+def dibagi_dua(x):
+    return divide()
 
 class TestApp:
     def test_multiplication(self, numbers):
