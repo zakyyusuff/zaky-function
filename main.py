ebaae27bbc3fd14606d42bd9d980a852c277be56
zakyyusuff <zakymuhamamdyusuf@gmail.com>
2022-11-10 11:14:52 +0700

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+
+from tests.tests_app import dikalikan_dua, dibagi_dua
+
+
+def main():
+    num = int(input("masukan angka: "))
+    print("kelipatan dari %d adalah %d" % (num, dikalikan_dua(num)))
+    print("setengah dari %d adalah %d" % (num, dibagi_dua(num)))
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/tests_app.py
@@ -0,0 +1,27 @@
+import pytest
+# from tests import dikalikan_dua, dibagi_dua
+from tests.mymodule.funcs import multiply, divide
+
+
+def dikalikan_dua(x):
+    return multiply(x, 2)
+
+
+def dibagi_dua(x):
+    return divide(x, 2)
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    return [a,b]
+
+
+class TestApp:
+    def test_multiplication(self, numbers):
+        res = dikalikan_dua(numbers[0])
+        assert res == numbers[1]
+
+    def test_division(self, numbers):
+        res = dibagi_dua(numbers[1])
+        assert res == numbers[0]
